37a8ce3d2e86eba540321ba35ca53695f36b5300
Yaroslav Torzuk <madhunyt@gmail.com>
2021-09-13 21:29:26 +0300

    Add NodeBase class

---
+++ src/node_base.cpp
@@ -0,0 +1,158 @@
+#include "node_base.h"
+#include <list>
+
+namespace yt_ml_toolkit {
+namespace graph_representation {
+
+Node::Node(const std::vector<TensorDescriptor::WeakPtr> &inputs, const std::string &name) :
+    name_ {name},
+    inputs_ {inputs.begin(), inputs.end()}
+{
+    for (auto &input : inputs_)
+        if (!input.expired())
+            input.lock()->consumers().push_back(this);
+}
+
+Node::Node(std::vector<TensorDescriptor::WeakPtr> &&inputs, const std::string &name) :
+    name_ {name},
+    inputs_ {std::move(inputs)}
+{
+    for (auto &input : inputs_)
+        if (!input.expired())
+            input.lock()->consumers().push_back(this);
+}
+
+Node::~Node()
+{
+    for (auto input : inputs_)
+    {
+        if (!input.expired())
+        {
+            auto &consumers = input.lock()->consumers();
+            consumers.erase(std::remove_if(consumers.begin(), consumers.end(), [this](Node* node) { return node == this; }),
+                    consumers.end());
+        }
+    }
+}
+
+const std::__cxx11::string &Node::name() const
+{
+    return name_;
+}
+
+Node::OutputsList &Node::outputs()
+{
+    return outputs_;
+}
+
+const Node::OutputsList &Node::outputs() const
+{
+    return outputs_;
+}
+
+Node::InputsList &Node::inputs()
+{
+    return inputs_;
+}
+
+const Node::InputsList &Node::inputs() const
+{
+    return inputs_;
+}
+
+yt_ml_toolkit::graph_representation::Node::operator TensorDescriptor::WeakPtr()
+{
+    return outputs_.front();
+}
+
+Node::operator TensorDescriptor::Ptr()
+{
+    return outputs_.front();
+}
+
+TensorDescriptor::TensorDescriptor(DataType dtype, Shape shape, Node *producer) :
+    dtype_ {dtype},
+    shape_ {shape},
+    producer_ {producer}
+{
+}
+
+TensorDescriptor::~TensorDescriptor()
+{
+    for (auto consumer : consumers_)
+    {
+        auto &inputsList = consumer->inputs();
+        inputsList.erase(std::remove_if(inputsList.begin(),
+                                        inputsList.end(),
+                                        [this](TensorDescriptor::WeakPtr ptr){ return ptr.lock().get() == this; }),
+                inputsList.end());
+    }
+}
+
+DataType TensorDescriptor::dataType() const
+{
+    return dtype_;
+}
+
+const Shape &TensorDescriptor::shape() const
+{
+    return shape_;
+}
+
+TensorDescriptor::NodesList &TensorDescriptor::consumers()
+{
+    return consumers_;
+}
+
+const TensorDescriptor::NodesList &TensorDescriptor::consumers() const
+{
+    return consumers_;
+}
+
+const Node* TensorDescriptor::producer() const
+{
+    return producer_;
+}
+
+Node *TensorDescriptor::producer()
+{
+    return producer_;
+}
+
+void backBFSTraversal(Node& node, std::function<void(Node&)> callback)
+{
+    std::list<Node*> list {&node};
+    auto current = list.front();
+    while (true)
+    {
+        callback(*current);
+        for (const auto &input : current->inputs())
+            list.push_back(input.lock()->producer());
+        list.pop_front();
+        if (list.empty())
+            break;
+        else
+            current = list.front();
+    }
+}
+
+void BFSTraversal(Node &node, std::function<void (Node &)> callback)
+{
+    std::list<Node*> list{&node};
+    auto current = &node;
+    while (true)
+    {
+        callback(*current);
+        for (const auto &output : current->outputs())
+            for (auto const& consumer : output->consumers())
+                list.push_back(consumer);
+        list.pop_front();
+        if (list.empty())
+            break;
+        else
+            current = list.front();
+    }
+}
+
+}
+}
---
+++ src/node_base.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "shape.h"
+#include <experimental/optional>
+#include <functional>
+#include <string>
+#include <vector>
+
+namespace yt_ml_toolkit {
+namespace graph_representation {
+
+class Node;
+
+class TensorDescriptor
+{
+public:
+    using Ptr = std::shared_ptr<TensorDescriptor>;
+    using WeakPtr = std::weak_ptr<TensorDescriptor>;
+    using NodesList = std::vector<Node*>;
+
+    TensorDescriptor(DataType dtype, Shape shape, Node* producer);
+    ~TensorDescriptor();
+    DataType dataType() const;
+    const Shape &shape() const;
+    NodesList &consumers();
+    const NodesList &consumers() const;
+    const Node* producer() const;
+    Node *producer();
+
+private:
+    DataType dtype_;
+    Shape shape_;
+    Node* producer_;
+    NodesList consumers_;
+};
+
+class Node : public std::enable_shared_from_this<Node>
+{
+public:
+    using OutputsList = std::vector<TensorDescriptor::Ptr>;
+    using InputsList = std::vector<TensorDescriptor::WeakPtr>;
+    using Ptr = std::shared_ptr<Node>;
+
+    explicit Node(const std::vector<TensorDescriptor::WeakPtr> &inputs, const std::string &name);
+    explicit Node(std::vector<TensorDescriptor::WeakPtr> &&inputs, const std::string &name);
+    virtual ~Node();
+    const std::string &name() const;
+    virtual const OutputsList &inferOutputShapes() = 0;
+    OutputsList& outputs();
+    const OutputsList &outputs() const;
+    InputsList& inputs();
+    const InputsList &inputs() const;
+    virtual operator TensorDescriptor::Ptr();
+    virtual operator TensorDescriptor::WeakPtr();
+
+protected:
+    std::string name_;
+    InputsList inputs_;
+    // To be initialized in a subclass
+    OutputsList outputs_;
+};
+
+void BFSTraversal(Node& node, std::function<void(Node&)> callback);
+void backBFSTraversal(Node& node, std::function<void(Node&)> callback);
+
+} // graph_representation
+} // yt_ml_toolkit
