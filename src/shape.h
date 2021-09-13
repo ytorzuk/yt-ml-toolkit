8de8eb3c9f305548aeb61e36833f72283b8a14ef
Yaroslav Torzuk <madhunyt@gmail.com>
2021-09-13 21:29:03 +0300

    Add shape class

---
+++ src/shape.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <array>
+#include <memory>
+
+namespace yt_ml_toolkit {
+
+enum DataType
+{
+    fp16,
+    fp32,
+    int8,
+    int16,
+    int32,
+    int64,
+    uint8,
+    uint16,
+    uint32,
+    uint64,
+};
+
+class Shape : public std::array<std::size_t, 5>
+{};
+
+} // yt_ml_toolkit
